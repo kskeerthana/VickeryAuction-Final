b91b3365a80aab1ea2dc16f7445fe656ce3123e0
Swaroop <giri.sw@northeastern.edu>
2023-12-15 11:47:35 -0500

    added create auction

--- vickery-auction-ui/src/components/AuctionCreation/CreateAuction.tsx
+++ vickery-auction-ui/src/components/AuctionCreation/CreateAuction.tsx
@@ -3,7 +3,7 @@ import './CreateAuction.css';
 import { ethers } from 'ethers';
 import contractsData from '../../contractsConfig.json';
 import VickreyAuction from '../../VickeryAuction.json';
-const yourContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; 
+const yourContractAddress = "0x822dF9c648F1D6A25c632908C7Eb1968531F1Ef4"; 
 
 
 function CreateAuction() {
@@ -23,46 +23,71 @@ function CreateAuction() {
     setSelectedErc20(e.target.value);
   };
   
-  const deployAuction = async () => {
+  const createAuction = async () => {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
-      const bytecode = "";
-      const VickeryAuction = new ethers.ContractFactory(VickreyAuction.abi, VickreyAuction.bytecode, signer);
+      const contract = new ethers.Contract(yourContractAddress, VickreyAuction.abi, signer);
       try {
-        const tokenId = document.getElementById("tokenId") as HTMLInputElement;
-        const startTime = document.getElementById("startTime") as HTMLInputElement;
-        const bidPeriod = document.getElementById("bidPeriod") as HTMLInputElement;
-        const revealPeriod = document.getElementById("revealPeriod") as HTMLInputElement;
-        const reservePrice = document.getElementById("reservePrice") as HTMLInputElement;
+        // const tokenId = document.getElementById("tokenId") as HTMLInputElement;
+        // const startTime = document.getElementById("startTime") as HTMLInputElement;
+        // const bidPeriod = document.getElementById("bidPeriod") as HTMLInputElement;
+        // const revealPeriod = document.getElementById("revealPeriod") as HTMLInputElement;
+        // const reservePrice = document.getElementById("reservePrice") as HTMLInputElement;
 
         // const tokenID = tokenId.value;
         // const auctionStartTime = startTime.value;
         // const autionBidPeriod = bidPeriod.value;
         // const auctionRevealPeriod = revealPeriod.value;
         // const auctionReservePrice = reservePrice.value;
+
         
-        const tokenID = 1;
-        const auctionStartTime = Date.now() + 60;
-        const autionBidPeriod = 60;
-        const auctionRevealPeriod = 60;
-        const auctionReservePrice = 10e5;
+        let tokenID = 1;
+        const auctiontokenID = ethers.utils.defaultAbiCoder.encode(['uint256'], [tokenID]);
+        let StartTime = 60;
+        const auctionStartTime = ethers.utils.defaultAbiCoder.encode(['uint32'], [StartTime]);
+        let BidPeriod = 60;
+        const auctionBidPeriod = ethers.utils.defaultAbiCoder.encode(['uint32'], [BidPeriod]);
+        let RevealPeriod = 60;
+        const auctionRevealPeriod = ethers.utils.defaultAbiCoder.encode(['uint32'], [RevealPeriod]);
+        let ReservePrice = 1000;
+        const auctionReservePrice = ethers.utils.defaultAbiCoder.encode(['uint96'], [ReservePrice]);
+        const auctionselectedErc721 = ethers.utils.defaultAbiCoder.encode(['address'], [selectedErc721]);
+        const auctionselectedErc20 = ethers.utils.defaultAbiCoder.encode(['address'], [selectedErc20]);
 
-        const basicVickeryAuction = await VickeryAuction.deploy();
+        console.log(auctiontokenID, auctionStartTime, auctionBidPeriod, auctionRevealPeriod, auctionReservePrice, auctionselectedErc721, auctionselectedErc20)
 
-        const deployedAddress = basicVickeryAuction.address;
-        window.alert(`Contract deployed to: ${deployedAddress}`);
-        setContractAddress(deployedAddress)
+        await contract.createAuction(auctionselectedErc721, auctiontokenID, auctionselectedErc20, auctionStartTime, auctionBidPeriod, auctionRevealPeriod, auctionReservePrice);
 
-        const deployed = document.getElementById("deployedAt") as HTMLInputElement;
-        deployed.textContent = `Deployed at ${deployedAddress}`
-
+        window.alert(`Auction created for: ${selectedErc721}`);
 
     } catch (error: any) {
         window.alert(
             'Error!' + (error && error.reason ? `\n\n${error.reason}` : `${error.message}`)
         );
-    }
-  };
+      }
+    };
+
+  const deployAuction = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const VickeryAuction = new ethers.ContractFactory(VickreyAuction.abi, VickreyAuction.bytecode, signer);
+    try {
+      const basicVickeryAuction = await VickeryAuction.deploy();
+
+      const deployedAddress = basicVickeryAuction.address;
+      window.alert(`Contract deployed to: ${deployedAddress}`);
+      setContractAddress(deployedAddress)
+
+      const deployed = document.getElementById("deployedAt") as HTMLInputElement;
+      deployed.textContent = `Deployed at ${deployedAddress}`
+
+
+  } catch (error: any) {
+      window.alert(
+          'Error!' + (error && error.reason ? `\n\n${error.reason}` : `${error.message}`)
+      );
+  }
+};
 
   return (
     <form onSubmit={handleSubmit}>
@@ -82,7 +107,8 @@ function CreateAuction() {
         ))}
       </select>
       
-      <button type="submit" onClick={deployAuction}>Create Auction</button>
+      <button type="submit" onClick={deployAuction}>Deploy Auction</button>
+      <button type="submit" onClick={createAuction}>Create Auction</button>
     </form>
   );
 }
